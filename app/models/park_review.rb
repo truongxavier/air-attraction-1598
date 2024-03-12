6141c501e475a07d785a035c4a24f82d38084493
Xavier Truong <truong.xavier@gmail.com>
2024-03-12 14:44:59 +0100

    air-attraction seed et park-review

--- app/models/park_review.rb
+++ app/models/park_review.rb
@@ -1,6 +1,5 @@
 class ParkReview < ApplicationRecord
   belongs_to :rental
-  belongs_to :user, through: :rental
 
   validates :comment, presence: true
   validates :rating, presence: true, numericality: { in: 0..5 }
