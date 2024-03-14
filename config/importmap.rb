31af6dcd58832497b43afa52208bd5310a892f54
Xavier Truong <truong.xavier@gmail.com>
2024-03-14 20:18:25 +0100

    air-attractshow show

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,3 +11,4 @@ pin "mapbox-gl" # @3.1.2
 pin "process" # @2.0.1
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.9/dist/flatpickr.js"
 pin "flatpickr/dist/plugins/rangePlugin", to: "flatpickr--dist--plugins--rangePlugin.js" # @4.6.13
+pin "flatpickr/dist/l10n/fr.js", to: "flatpickr--dist--l10n--fr.js.js" # @4.6.13
